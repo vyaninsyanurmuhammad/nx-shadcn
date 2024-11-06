6a0d0eba8ed8ec6c90f53abafd5962061f81143f
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-11-06 21:56:03 +0700

    refactor: remove console.log

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,3 @@
 import { buildConfig } from '../../packages/utils-lib/src/tailwind.config';
 
-console.log(__dirname)
-
-console.log('Tailwind config:', buildConfig(__dirname));
-
 export default buildConfig(__dirname);
